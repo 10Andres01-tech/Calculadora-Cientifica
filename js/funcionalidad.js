4d8a328ac968f3e3b0fde4d791f4da2e6f6250cf
10Andres01-tech <andresjimenez.c55@gmail.com>
2026-05-19 12:11:38 -0500

    Resolver conflicto de merge

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -6,26 +6,18 @@ let p = {
     cantidadSigno: 0,
     cantDecimal: false,
     resultado: false,
-<<<<<<< HEAD
-};
-
-=======
     borrar: document.querySelector("#borrar")
 };
 
-// Crear los métodos
->>>>>>> fad1aa00b31f705cafdddf5bee6d84c54cfc6ca4
 let m = {
 
     inicio: function () {
 
-<<<<<<< HEAD
         for (let i = 0; i < p.teclas.length; i++) {
-
             p.teclas[i].addEventListener("click", m.oprimirTecla);
-
         }
 
+        p.borrar.addEventListener("click", m.limpiar);
     },
 
     oprimirTecla: function (e) {
@@ -34,7 +26,6 @@ let m = {
         p.digito = e.target.innerHTML;
 
         m.calculadora(p.accion);
-
     },
 
     calculadora: function (accion) {
@@ -42,98 +33,32 @@ let m = {
         switch (accion) {
 
             case "numero":
-
                 if (p.operaciones.innerHTML == "0") {
-
                     p.operaciones.innerHTML = p.digito;
-
                 } else {
-
                     p.operaciones.innerHTML += p.digito;
-
                 }
-
                 break;
 
             case "simbolo":
-
                 p.operaciones.innerHTML += p.digito;
-
                 break;
 
             case "decimal":
-
                 p.operaciones.innerHTML += p.digito;
-
                 break;
 
             case "igual":
-
                 p.operaciones.innerHTML = eval(p.operaciones.innerHTML);
-
-                break;
-
-        }
-
-    },
-    borrarCalculadora: function(){
-        p.operaciones.innerHTML = 0;
-    }
-
-};
-
-=======
-        // Eventos para las teclas
-        for (let i = 0; i < p.teclas.length; i++) {
-            p.teclas[i].addEventListener("click", m.oprimirTecla);
-        }
-
-        // Evento borrar
-        p.borrar.addEventListener("click", m.limpiar);
-    },
-
-    oprimirTecla: function (tecla) {
-
-        p.accion = tecla.target.getAttribute("class");
-        p.digito = tecla.target.innerHTML;
-
-        m.calculadora(p.accion, p.digito);
-    },
-
-    calculadora: function (accion, digito) {
-
-        switch (accion) {
-
-            
-            case "numero":
-                    console.log("numero");
-            break;
-
-           
-            case "simbolo":
-                    console.log("numero");
-                break;
-
- 
-            case "decimal":
-                    console.log("decimal");
-                break;
-
-
-            case "igual":
-                    console.log("igual");
                 break;
         }
     },
 
     limpiar: function () {
-
         p.operaciones.innerHTML = "0";
         p.cantDecimal = false;
         p.resultado = false;
     }
 };
 
-
->>>>>>> fad1aa00b31f705cafdddf5bee6d84c54cfc6ca4
 m.inicio();
